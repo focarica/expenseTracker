06949296b4323c49573aaaeb05bb3f7b8046c161
focarica <a.vinicius0042@gmail.com>
2024-09-23 15:24:39 -0300

    comandos e argumentos do arquivo principal criado

---
+++ main.py
@@ -0,0 +1,43 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog='expense-tracker'
+)
+
+subparsers = parser.add_subparsers(dest='command')
+
+# commands
+addParser = subparsers.add_parser('add', help='Add a new expense.')
+addParser.add_argument('--description', 
+                       type=str, 
+                       help='Description of the new expense.')
+addParser.add_argument('--amount', 
+                       type=int, 
+                       help='Amount of the new expense.')
+
+
+listParser = subparsers.add_parser('list', help='List all expenses.')
+
+
+summaryParser = subparsers.add_parser('summary', help='Summary of all expenses.')
+summaryParser.add_argument('--month',
+                           type=int,
+                          help="Filter summary to specific month")
+
+
+deleteParser = subparsers.add_parser('delete', help='Delete a expense.')
+deleteParser.add_argument('--id',
+                          type=int,
+                          help='Id of expense to delete.')
+
+args = parser.parse_args()
+
+match args.command:
+    case 'add':
+        pass
+    case 'list':
+        pass
+    case 'summary':
+        pass
+    case 'delete':
+        pass
