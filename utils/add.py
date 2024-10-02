e76ae7b194b753dcc9d7becc8e7aa9b8c8994ec7
focarica <a.vinicius0042@gmail.com>
2024-10-01 22:30:16 -0300

    .

--- utils/add.py
+++ utils/add.py
@@ -1,16 +1,16 @@
-import datetime
-
 from utils.jsonFunctions import loadExpenses, writeExpense
 
 
-'''def isInExpenses(description: str, expenses: list) -> int | None:
-    for expense in expenses:
-        if description == expense["description"]:
-            return expense['id'] - 1
-
-    return None'''
+# remake
+def isInExpenses(description: str, expenses: list) -> int | None:
+    for i in range(len(expenses)):
+        if expenses[i]["description"] == description:
+            return i
+    return None
 
 def addExpense(description: str, amount: int) -> str:
+    ret = ""
+    
     if amount < 0:
         return "Plese add a valid amount."
     if description == '':
@@ -18,13 +18,19 @@ def addExpense(description: str, amount: int) -> str:
     
     
     expenses = loadExpenses()
-    # idAlreadyInExpenses = isInExpenses(description=description, expenses=expenses)
     
-    expenses.append(
-        {"id":len(expenses)+1, 
-         "description":description, 
-         "amount": amount})
+    idExpenseAlreadyAdd = isInExpenses(description=description, expenses=expenses)
+
+    newExpense = {"id": '''to make''', "description":description, "amount": amount}
     
+    if(idExpenseAlreadyAdd):
+        expenses.remove('''to make''')
+        expenses.insert(idExpenseAlreadyAdd+1, newExpense)    
+        
+        ret = f"Updated amount of {description} expense."
+    else:
+        expenses.append(newExpense)            
+        ret = f"Added new expense with id {'''to make'''}."
+        
     writeExpense(expense=expenses)
-    
-    return f"Added new expense with id {len(expenses)}."
+    return ret
